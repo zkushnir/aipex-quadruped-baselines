e310470c16654bf5c1533eb6ecd77da6dfb99a3e
Zach Kushnir <152830421+zkushnir@users.noreply.github.com>
2025-11-29 14:37:46 -0500

    Update convex_mpc_node to use external ref trajectory

--- src/convex_mpc/src/convex_mpc_node.cpp
+++ src/convex_mpc/src/convex_mpc_node.cpp
@@ -6,7 +6,6 @@
 #include <stdexcept>
 #include <Eigen/Dense>
 
-#include "std_msgs/msg/float64_multi_array.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -72,24 +71,11 @@ QuadConvexMPCNode::QuadConvexMPCNode()
 
     joint_torque_pub_ = this->create_publisher<unitree_go::msg::LowCmd>("/lowcmd", 10);
     publish_joint_torque_timer_ = this->create_wall_timer(std::chrono::milliseconds(JOINT_TORQUE_PUBLISH_RATE_MS), std::bind(&QuadConvexMPCNode::publish_cmd, this));
-	
-	//x0 publish
-	x0_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/x0", 10);
 
     // Create a lowstate subscriber to update mpc states
     low_state_sub_ = this->create_subscription<unitree_go::msg::LowState>(
         "lowstate", 1, std::bind(&QuadConvexMPCNode::low_state_callback, this, std::placeholders::_1)
     );
-
-    // Publish current x0 so another node can generate a reference
-    x0_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/x0", 10);
-
-    // Subscribe to reference trajectory from external node
-    ref_sub_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
-        "/reference_trajectory", 10,
-        std::bind(&QuadConvexMPCNode::refCallback, this, std::placeholders::_1)
-    );
-
 
     // Sport mode state subscriber
     // TODO: switch between subscriber callbacks based on measurement_mode
